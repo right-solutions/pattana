604964c3ea36cab99a56450333587f1f13ddcb7f
Krishna Prasad Varma <prasad@rightsolutions.io>
2017-08-21 09:55:17 +0400

    Modified the engine file

--- lib/pattana/engine.rb
+++ lib/pattana/engine.rb
@@ -1,5 +1,24 @@
 module Pattana
   class Engine < ::Rails::Engine
+    
+    require 'kaminari'
+    require 'kuppayam'
+    require 'colorize'
+    require 'colorized_string'
+    # require 'pry'
+    
     isolate_namespace Pattana
+
+    config.before_initialize do                                                      
+      config.i18n.load_path += Dir["#{config.root}/config/locales/**/*.yml"]
+    end
+
+    config.generators do |g|
+      g.test_framework      :rspec,        :fixture => false
+      g.fixture_replacement :factory_girl, :dir => 'spec/dummy/spec/factories'
+      g.assets false
+      g.helper false
+    end
+    
   end
 end
